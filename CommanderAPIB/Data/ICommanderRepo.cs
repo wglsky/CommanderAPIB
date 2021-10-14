e9266576192f2ff5c7281c3007d8d041bf4b8954
wglsky <40151406+wglsky@users.noreply.github.com>
2021-10-14 21:50:39 +0200

    Added Interface ICommandRepo

---
+++ CommanderAPIB/Data/ICommanderRepo.cs
@@ -0,0 +1,15 @@
+﻿using CommanderAPIB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommanderAPIB.Data
+{
+    interface ICommanderRepo
+    {
+        IEnumerable<Command> GetAppCommands();
+        Command GetCommandById(int id);
+
+    }
+}
